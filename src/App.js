f45826808a7fabf19a74623b51e1e95a0bd83815
Erickcamp <thecampkid@gmail.com>
2020-05-20 19:47:41 -0600

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isMobileMenuOpen: false,
+    };
+  }
+
+  toggleMobileMenu = () => {
+    this.setState({
+      isMobileMenuOpen: !this.state.isMobileMenuOpen,
+    });
+  };
+  render() {
+    console.log(this.state.isMobileMenuOpen);
+    return (
+      <div className="App">
+        <header className="header">
+          <div className="Charlene-said-so">
+            <div className="logo-area">
+              <img
+                className="start"
+                src="https://blackrockdigital.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"
+                alt="start bootstrap"
+              />
+            </div>
+            <nav className="nav">
+              <ul className="nav-list">
+                <li className="nav-list-item">
+                  <a href="link">Services</a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="link">Portfolio</a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="link">About</a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="link">Team</a>
+                </li>
+                <li className="nav-list-item">
+                  <a href="link">Contact</a>
+                </li>
+              </ul>
+            </nav>
+            <nav className="mobile-nav">
+              <p className="menu" onClick={this.toggleMobileMenu}>
+                Menu
+              </p>
+              {/* <img
+                className="hamburger"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Hamburger_icon.svg/1200px-Hamburger_icon.svg.png"
+                alt="hamburger"
+                onClick={this.toggleMobileMenu}
+              /> */}
+              {this.state.isMobileMenuOpen && (
+                <ul>
+                  <li className="mobile-nav-list-item">Services</li>
+                  <li className="mobile-nav-list-item">Portfolio</li>
+                  <li className="mobile-nav-list-item">About</li>
+                  <li className="mobile-nav-list-item">Team</li>
+                  <li className="mobile-nav-list-item">Contact</li>
+                </ul>
+              )}
+            </nav>
+          </div>
+          <div className="welcome">
+            <h1>
+              <span className="welcome-text">Welcome To Our Studio!</span>
+              <br />
+              <span className="greeting-text">IT'S NICE TO MEET YOU</span>
+              <br />
+            </h1>
+            <a className="more-text" href="more">
+              TELL ME MORE
+            </a>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
